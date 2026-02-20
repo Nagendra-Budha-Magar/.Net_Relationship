59057ff6bc7f31ad354006f4141c0f30e951aa35
Ashish_Budha <nagendrabudha47@gmail.com>
2026-02-21 00:58:19 +0545

    Jwt Token Services

---
+++ practicing/Domain/Dtos/UserDto.cs
@@ -0,0 +1,16 @@
+﻿namespace practicing.Domain.Dtos
+{
+    public class RegisterDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; } = "User";
+    }
+
+    public class LoginDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+}
