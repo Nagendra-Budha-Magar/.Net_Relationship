162d08ab6dcd57a4204e7151a8adfbc1555e20ba
Ashish_Budha <nagendrabudha47@gmail.com>
2026-02-20 20:58:29 +0545

    Register security rule

--- practicing/Program.cs
+++ practicing/Program.cs
@@ -1,8 +1,11 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using practicing.Application.Repositories;
 using practicing.Application.Services;
 using practicing.Data;
+using System.Text;
 
 namespace practicing
 {
@@ -16,11 +19,36 @@ namespace practicing
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+            // Register the "Security Rules"
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(options =>
+                {
+                    options.RequireHttpsMetadata = false;
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+                        ValidateIssuer = true,
+                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                        ValidateAudience = true,
+                        ValidAudience = builder.Configuration["Jwt:Audience"],
+                        ValidateLifetime = true,
+                        ClockSkew = TimeSpan.Zero
+                    };
+                });
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             builder.Services.AddDbContext<AppDbContext>(options =>
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+            // Register Services and Register
             builder.Services.AddScoped<IStudentService, StudentService>();
             builder.Services.AddScoped<IAssignSubjectService, AssignSubjectService>();
             builder.Services.AddScoped<IAssignSubjectRepository, AssignSubjectRepository>();
